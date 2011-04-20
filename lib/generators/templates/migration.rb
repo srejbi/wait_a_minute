bfca919730913ccd84265a83684514e6a0443f00
Gyuri Schreiber <george@digitalsmile.com>
2011-04-20 13:02:08 +0200

    migration fix

--- lib/generators/templates/migration.rb
+++ lib/generators/templates/migration.rb
@@ -1,4 +1,4 @@
-class CreateWaitAMinuteRequestLogs < ActiveRecord::Migration
+class WaitAMinuteCreateWaitAMinuteRequestLogs < ActiveRecord::Migration
   def self.up
     create_table :wait_a_minute_request_logs, :id => false do |t|
       t.string :ip
@@ -11,9 +11,9 @@ class CreateWaitAMinuteRequestLogs < ActiveRecord::Migration
     add_index :wait_a_minute_request_logs, :ip
     #add_index :wait_a_minute_request_logs, :url
     add_index :wait_a_minute_request_logs, :created_at
-    add_index :wait_a_minute_request_logs, [:ip, :refused]
-    add_index :wait_a_minute_request_logs, [:ip, :created_at]
-    add_index :wait_a_minute_request_logs, [:ip, :created_at, :refused]
+    add_index :wait_a_minute_request_logs, [:ip, :refused], :name => 'refused_ip_index'
+    add_index :wait_a_minute_request_logs, [:ip, :created_at], :name => 'ip_by_date_index'
+    add_index :wait_a_minute_request_logs, [:ip, :created_at, :refused] => 'index_all'
   end
 
   def self.down
--- lib/generators/wait_a_minute/migration_generator.rb
+++ lib/generators/wait_a_minute/migration_generator.rb
@@ -7,7 +7,7 @@ module WaitAMinute
       class_option :orm
 
       def copy_migration
-        template 'migration.rb', "db/migrate/#{Time.new.strftime("%Y%m%d%H%M%S")}_create_wait_a_minute.rb"
+        template 'migration.rb', "db/migrate/#{Time.new.strftime("%Y%m%d%H%M%S")}_wait_a_minute_create_wait_a_minute_request_logs.rb"
       end
     end
   end
